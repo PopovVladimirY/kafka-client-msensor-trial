910873c25e1f1d2541c47ba7f15ab34c4961bd89
Vladimir POPOV <vladimir.popov@free.fr>
2023-11-02 23:12:58 +0100

    subscribing to topic pattern

--- consumer.py
+++ consumer.py
@@ -5,14 +5,15 @@ from avro.io import DatumReader
 import datetime 
 
 broker='192.168.200.12:9092'
-client_id = 'test'
-msensor1_topic = "msensor1_live_topic"
+client_id = 'test-msensor'
+msensor_topic = "msensor_live_topic"
 
 avro_schema = '''{
     "namespace": "msensor.avro",
     "type": "record",
     "name": "msensor",
     "fields": [
+        {"name": "device", "type": ["string", "null"]},
         {"name": "timestamp", "type": ["float", "null"]},
         {"name": "wakeup_count", "type": ["int", "null"]},
         {"name": "temperature",  "type": ["float", "null"]},
@@ -26,12 +27,12 @@ avro_schema = '''{
 schema = avro.schema.parse(avro_schema)
 
 # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer(msensor1_topic,
-                         group_id=client_id,
+consumer = KafkaConsumer(group_id=client_id,
                          bootstrap_servers=broker)
-#                        auto_offset_reset='earliest')
+#                        auto_offset_reset='earliest')g
 #                         ,enable_auto_commit=False)
 
+consumer.subscribe(pattern='^.*msensor_live_topic$')
 
 for msg in consumer:
     # message value and key are raw bytes -- decode if necessary!
@@ -45,6 +46,7 @@ for msg in consumer:
     print("\n%s:%d:%d: key=%s" % (msg.topic, msg.partition, msg.offset, msg.key))
 
     d = datetime.datetime.fromtimestamp(msensor['timestamp'])
+    print(f"     Device: {msensor['device']}")
     print(f"  Date/Time: {d}")
     print(f" Boot Count: {msensor['wakeup_count']}")
     print(f"Temperature: {msensor['temperature']:.1f} °C")
